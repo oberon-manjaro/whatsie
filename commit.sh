216791ad1650fc2c162bd46e09f30839768ca6fc
Alexandru Rosianu <me@aluxian.com>
2016-06-15 16:38:18 +0300

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
 git add .
 git commit -m "$1"
+git push
+
 gulp live build
-(cd ../gh-pages && git add . && git commit -m "$1" && git push)
+cd ../gh-pages
+git add .
+git commit -m "$1"
+#git push
